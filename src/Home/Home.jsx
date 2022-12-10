335361dd84624592c1b6e5146b23d2e85d2863a3
ErikaJPB <erikajpinedab@gmail.com>
2022-12-09 22:34:54 -0500

    Se agregaron estilos para las tarjetas del Home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../redux/actions";
-import Carousel from "./Carousel/Carousel";
-import Card from "./Card"
+import Carousel from "../components/Carousel/Carousel";
+import Card from "../components/Card/Card";
+import "./Home.css";
 
 function Home() {
   const allProducts = useSelector((state) => state.Products);
@@ -10,24 +11,24 @@ function Home() {
 
   useEffect(() => {
     dispatch(getAllProducts());
-  },[dispatch]);
+  }, [dispatch]);
 
   return (
     <div>
       <Carousel />
-      {allProducts?.slice(0,5).map((e) => (
-          <div key={e.id}>
+      {allProducts?.slice(0, 10).map((e) => {
+        return (
+          <div className="product-card" key={e.id}>
             <Card
               id={e.id}
               name={e.name}
-              brand={e.brand}
               price={e.price}
-              stock={e.stock}
               rating={e.rating}
               image={e.image}
             />
           </div>
-        ))}
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Card.css";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <h3 className="card-title">{props.name}</h3>
+
+      <h5 className="card-price">
+        {" "}
+        Precio: <p>{props.price}</p>
+      </h5>
+
+      <img
+        src={props.image}
+        alt="image not found"
+        width={"100px"}
+        height={"160px"}
+      />
+
+      <h5>
+        {" "}
+        Proximamente estrellas?<p>{props.rating}</p>
+      </h5>
+    </div>
+  );
+}
+
+export default Card;
