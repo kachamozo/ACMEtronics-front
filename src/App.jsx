8e5685cb3cc72aea4f790e5880f049604f803d19
ErikaJPB <erikajpinedab@gmail.com>
2022-12-11 12:21:45 -0500

    Agregar productos con algo de estilo pero sin funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import About from "./components/About/About";
 import Shop from "./components/Shop/Shop";
 import Detail from "./components/Detail/Detail";
 import Carousel from "./components/Carousel/Carousel";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <div>
       <Navbar />
-     
+
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/shop/:id" element={<Detail />} />
+        <Route path="/shop/add" element={<Form />} />
       </Routes>
     </div>
   );
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,47 +1,138 @@
 import React from "react";
-import { Formik, Form } from "formik";
+import { Formik } from "formik";
+import "./Form.css";
 
-function Form() {
+function AddForm() {
   return (
     <>
-      <form className="form">
-        <div>
-          <label htmlFor="name">Nombre: </label>
-          <input type="text" id="name" placeholder="Nombre del producto" />
-        </div>
-        <div>
-          <label htmlFor="description">Descripcion: </label>
-          <input
-            type="text"
-            id="description"
-            placeholder="Descripcion del producto"
-          />
-        </div>
-        <div>
-          <label htmlFor="brand">Marca: </label>
-          <input type="text" id="name" placeholder="Nombre del producto" />
-        </div>
-        <div>
-          <label htmlFor="price">Precio: </label>
-          <input type="text" id="price" placeholder="Precio del producto" />
-        </div>
-        <div>
-          <label htmlFor="stock">Cantidadades Disponibles: </label>
-          <input type="text" id="stock" placeholder="Cantidades disponibles" />
-        </div>
-        <div>
-          <label htmlFor="image">Imágen del producto: </label>
-          <input
-            type="text"
-            id="image"
-            placeholder="Inserta la imágen del producto"
-          />
-        </div>
+      <h1 className="title">Agregar Producto</h1>
+      <Formik
+        initialValues={{
+          name: "",
+          description: "",
+          brand: "",
+          price: "",
+          stock: "",
+          image: "",
+        }}
+        validate={(values) => {
+          let error = {};
+          if (!values.name) {
+            error.name = "Debes ingresar el nombre del producto.";
+          }
 
-        <button type="submit">Agregar producto</button>
-      </form>
+          if (!values.brand) {
+            error.brand = "Debes ingresar la marca del producto.";
+          }
+
+          if (!values.price) {
+            error.price = "Debes ingresar el precio del producto.";
+          }
+
+          if (!values.stock) {
+            error.stock = "Debes ingresar las cantidades disponibles.";
+          }
+
+          if (!values.image) {
+            error.image = "Debes ingresar la imágen del producto.";
+          }
+
+          if (!values.description) {
+            error.description = "Debes ingresar una descripción del producto.";
+          }
+          return error;
+        }}
+        onSubmit={() => {}}
+      >
+        {({ values, handleSubmit, handleChange, handleBlur, errors }) => (
+          <form className="form" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Nombre: </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Nombre del producto"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.name && <div className="error">{errors.name} </div>}
+            </div>
+
+            <div>
+              <label htmlFor="brand">Marca: </label>
+              <input
+                type="text"
+                id="brand"
+                placeholder="Nombre del producto"
+                value={values.brand}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.brand && <div className="error">{errors.brand} </div>}
+            </div>
+
+            <div>
+              <label htmlFor="price">Precio: </label>
+              <input
+                type="number"
+                id="price"
+                placeholder="Precio del producto"
+                value={values.price}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.price && <div className="error">{errors.price} </div>}
+            </div>
+            <div>
+              <label htmlFor="stock">Cantidadades Disponibles: </label>
+              <input
+                type="number"
+                id="stock"
+                placeholder="Cantidades disponibles"
+                value={values.stock}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.stock && <div className="error">{errors.stock} </div>}
+            </div>
+            <div>
+              <label htmlFor="image">Imágen del producto: </label>
+              <input
+                type="text"
+                id="image"
+                placeholder="Inserta la imágen del producto"
+                value={values.image}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.image && <div className="error">{errors.image} </div>}
+            </div>
+
+            <div>
+              <label htmlFor="description">Descripcion: </label>
+
+              <textarea
+                className="description"
+                id="description"
+                cols="24"
+                rows="10"
+                placeholder="Description"
+                values={values.description}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              ></textarea>
+              {errors.description && (
+                <div className="error">{errors.description} </div>
+              )}
+            </div>
+
+            <button type="submit">Agregar producto</button>
+          </form>
+        )}
+      </Formik>
     </>
   );
 }
 
-export default Form;
+export default AddForm;
