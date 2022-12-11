6582a893c8d2f03e267911f324a7d9f34d2ff7e8
ErikaJPB <erikajpinedab@gmail.com>
2022-12-11 12:53:53 -0500

    actions - reducer

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
-export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
-export const GET_PRODUCT_DETAIL = 'GET_PRODUCT_DETAIL';
-export const SEARCH_NAME = 'SEARCH_NAME'
+import axios from "axios";
+export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+export const GET_PRODUCT_DETAIL = "GET_PRODUCT_DETAIL";
+export const SEARCH_NAME = "SEARCH_NAME";
+export const GET_CATEGORIES = "GET_CATEGORIES";
 
 export const getAllProducts = () => {
   return async function (dispatch) {
-    const response = await axios.get('http://localhost:3001/product');
+    const response = await axios.get("http://localhost:3001/product");
     //console.log(response.data)
     return dispatch({
       type: GET_ALL_PRODUCTS,
@@ -13,20 +14,53 @@ export const getAllProducts = () => {
     });
   };
 };
-/* ---- GET PRODUCT BY ID - descomentar cuando esté la ruta -------
-export const getProductDetail = (id) =>{
-  return async function(dispatch){
-    const response = await axios.get('http://localhost:3001/product/:'+id)
+
+export const getCategories = () => {
+  return async function (dispatch) {
+    return await axios
+      .get("http://localhost:3001/category")
+      .then((response) => {
+        dispatch({
+          type: GET_CATEGORIES,
+          payload: response.data,
+        });
+      });
+  };
+};
+
+//  ---- GET PRODUCT BY ID - descomentar cuando esté la ruta -------
+export const getProductDetail = (id) => {
+  return async function (dispatch) {
+    const response = await axios.get("http://localhost:3001/product/:" + id);
     return dispatch({
       type: GET_PRODUCT_DETAIL,
-      payload: response.data
-    })
-  }
-} */
+      payload: response.data,
+    });
+  };
+};
+
+export const ClearDetail = () => {
+  return {
+    type: "CLEAR_DETAIL",
+  };
+};
+
+export const createProduct = (payload) => {
+  return async function (dispatch) {
+    const response = await axios.post("http://localhost:3001/product", payload);
+    return response;
+  };
+};
 
 export const searchName = (name) => {
-  return {
-    type: SEARCH_NAME,
-    payload: name
-  }
-}
+  return async function (dispatch) {
+    return await axios
+      .get(`http://localhost:3001/product?name=${name}`)
+      .then((response) => {
+        dispatch({
+          type: SEARCH_NAME,
+          payload: response.data,
+        });
+      });
+  };
+};
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,35 +1,47 @@
 import { GET_ALL_PRODUCTS, GET_PRODUCT_DETAIL, SEARCH_NAME } from "../actions";
 const initialState = {
-	Products: [], 
-	detail: [],
-	copyProducts: []
+  Products: [],
+  detail: [],
+  copyProducts: [],
 };
 
 function rootReducer(state = initialState, action) {
-	switch (action.type) {
-		case GET_ALL_PRODUCTS:
-			return {
-				...state,
-				Products: action.payload,
-				copyProducts: action.payload
-			}
-		/* case GET_PRODUCT_DETAIL: 
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return {
+        ...state,
+        Products: action.payload,
+        copyProducts: action.payload,
+      };
+    /* case GET_PRODUCT_DETAIL: 
 			return {
 				...state,
 				detail: action.payload
 			} */
-		case SEARCH_NAME: {
-			const all = state.Products
-			const search = all.filter(f => f.name.toLowerCase().includes(action.payload.toLowerCase()))
-			//console.log(state.copy)
-			return {
-				...state,
-				copyProducts: search
-			}
-		}
-		default:
-			return {...state}
-	}
+    case SEARCH_NAME: {
+      const all = state.Products;
+      const search = all.filter((f) =>
+        f.name.toLowerCase().includes(action.payload.toLowerCase())
+      );
+      //console.log(state.copy)
+      return {
+        ...state,
+        copyProducts: search,
+      };
+    }
+
+    case CREATE_PRODUCT:
+      return { ...state };
+
+    case "CLEAR_DETAIL":
+      return {
+        ...state,
+        detail: [],
+      };
+
+    default:
+      return { ...state };
+  }
 }
 
 export default rootReducer;
