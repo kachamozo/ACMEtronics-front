e757509de3010ade5aef7d5c37bd2f31f950d292
ErikaJPB <erikajpinedab@gmail.com>
2022-12-11 17:36:19 -0500

    actions-reducer-form

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
-export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
-export const GET_PRODUCT_DETAIL = 'GET_PRODUCT_DETAIL';
-export const SEARCH_NAME = 'SEARCH_NAME'
-export const CLEAN = "CLEAN"
+import axios from "axios";
+export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+export const GET_PRODUCT_DETAIL = "GET_PRODUCT_DETAIL";
+export const SEARCH_NAME = "SEARCH_NAME";
+export const CLEAN = "CLEAN";
 
 export const getAllProducts = () => {
   return async function (dispatch) {
-    const response = await axios.get('http://localhost:3001/product');
-    
+    const response = await axios.get("http://localhost:3001/product");
+
     return dispatch({
       type: GET_ALL_PRODUCTS,
       payload: response.data.products,
@@ -29,12 +29,37 @@ export const getProductDetail = (id) =>{
 export const searchName = (name) => {
   return {
     type: SEARCH_NAME,
-    payload: name
-  }
-}
+    payload: name,
+  };
+};
 export const clean = (payload) => {
   return {
     type: CLEAN,
     payload,
-  }
-}
+  };
+};
+export const getCategories = () => {
+  return async function (dispatch) {
+    return await axios
+      .get("http://localhost:3001/category")
+      .then((response) => {
+        dispatch({
+          type: "GET_CATEGORIES",
+          payload: response.data,
+        });
+      });
+  };
+};
+
+export const ClearDetail = () => {
+  return {
+    type: "CLEAR_DETAIL",
+  };
+};
+
+export const createProduct = (payload) => {
+  return async function (dispatch) {
+    const response = await axios.post("http://localhost:3001/product", payload);
+    return response;
+  };
+};
