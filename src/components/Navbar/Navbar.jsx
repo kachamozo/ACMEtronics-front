fd2b6884bced425ce8a03da32cb21da685f3db0d
Kachamozo <cachamocito@yahoo.com>
2022-12-20 16:11:38 -0400

    auth0 1ra parte

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,73 +3,91 @@ import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { searchName } from '../../redux/actions';
 import './Navbar.css';
-import logo from '../../Assets/logo1.jpg'
+import logo from '../../Assets/logo1.jpg';
 import { Orderaz } from '../Order/Orderaz';
+import { Login } from '../Auth0/Login';
+import { Logout } from '../Auth0/Logout';
 
-
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Navbar() {
+	const dispatch = useDispatch();
+	const [name, setName] = useState('');
+	const handleSearch = (e) => {
+		dispatch(searchName(e));
+		setName(e);
+	};
 
-    const dispatch = useDispatch()
-    const [name, setName] = useState('')
-   const handleSearch = (e) => {
-    dispatch(searchName(e))
-    setName(e)
-   }
-    return ( 
-        <div className='nav-bar' >
-            <img className='logo' src={logo} alt='logo' width={"100px"} height={"100px"}/>
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/shop">Shop</NavLink>
-            <NavLink to="/about">About</NavLink>
-    
-        <div className='search-bar'>
-              <input
-              id='search'
-              className="bar-btn"
-              name="search"
-              placeholder="Search..."
+	const { isAuthenticated } = useAuth0();
+	return (
+		<div className='nav-bar'>
+			<img
+				className='logo'
+				src={logo}
+				alt='logo'
+				width={'100px'}
+				height={'100px'}
+			/>
+			<NavLink to='/home'>Home</NavLink>
+			<NavLink to='/shop'>Shop</NavLink>
+			<NavLink to='/about'>About</NavLink>
 
-              value={name}
-              onChange={(e) => handleSearch(e.target.value)}
-            ></input>
-            
-            <button className="btn" >
+			<div className='search-bar'>
+				<input
+					id='search'
+					className='bar-btn'
+					name='search'
+					placeholder='Search...'
+					value={name}
+					onChange={(e) => handleSearch(e.target.value)}
+				></input>
 
+				<button className='btn'>
+					<img
+						src='https://img.icons8.com/ios/50/000000/search--v1.png'
+						width={'30px'}
+						height={'30px'}
+					/>
+				</button>
+			</div>
 
+			<div className='cart'>
+				<button className='btnes'>
+					<img
+						src='https://img.icons8.com/ios/50/000000/like--v1.png'
+						width={'30px'}
+						height={'30px'}
+					/>
+				</button>
 
-              <img src="https://img.icons8.com/ios/50/000000/search--v1.png" width={"30px"} height={"30px"}/>
-            </button>
-           
-            </div>
-            
-            <div className="cart">
+				<button className='btnes'>
+					<img
+						src='https://img.icons8.com/ios/50/000000/shopping-cart--v1.png'
+						width={'30px'}
+						height={'30px'}
+					/>
+				</button>
 
-            <button className="btnes" >
-                <img src="https://img.icons8.com/ios/50/000000/like--v1.png" width={"30px"} height={"30px"}/>
-            </button>
-    
-            <button className="btnes" >
-                <img src="https://img.icons8.com/ios/50/000000/shopping-cart--v1.png" width={"30px"} height={"30px"}/>
-            </button>
-    
-            <button className="btnes" >
-                <img src="https://img.icons8.com/ios/50/000000/user--v1.png" width={"30px"} height={"30px"}/>
-            </button>
-<Orderaz/>
-            </div>
-    
-                
-    
-        </div>
-      );
-    } 
-    
-    
-    
-    export default Navbar;
+				<button className='btnes'>
+					<img
+						src='https://img.icons8.com/ios/50/000000/user--v1.png'
+						width={'30px'}
+						height={'30px'}
+					/>
+				</button>
 
+				{isAuthenticated ? (
+					<>
+						<Logout />
+					</>
+				) : (
+					<Login />
+				)}
 
+				<Orderaz />
+			</div>
+		</div>
+	);
+}
 
-
-    
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,24 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store/index';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
+//auth0
+
+// const DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
+// const CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
 		<BrowserRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+			<Auth0Provider
+				domain='dev-mdvrjlkwsu0q1u2j.us.auth0.com'
+				clientId='AYhTbnrpD7KIFtYsDRbx80zmhQfMpnsS'
+				redirectUri={window.location.origin}
+			>
+				<App />
+			</Auth0Provider>
 		</BrowserRouter>
 	</Provider>
 );
