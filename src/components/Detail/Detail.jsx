1ec29136a438c9d540b4994452bb3609a1da4fce
ErikaJPB <erikajpinedab@gmail.com>
2022-12-26 09:33:24 -0500

    fix-detail

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,74 +1,91 @@
 import React, { useEffect } from "react";
-import d from '../Detail/detail.module.css'
+import d from "../Detail/detail.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams  } from "react-router";
+import { useParams } from "react-router";
 import { getProductDetail, clean, addToCart } from "../../redux/actions";
 import { ToastContainer, toast } from "react-toastify";
 import { Reviews } from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+export default function Detail() {
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.detail);
+  const { id } = useParams();
 
-export default function Detail(){
-    const dispatch = useDispatch()
-    const product = useSelector((state) => state.detail)
-    const {id} = useParams()
-    
-    useEffect(()=> {
-      dispatch(getProductDetail(id))
-      return () => {dispatch(clean())}
-  }, [dispatch, id])
+  useEffect(() => {
+    dispatch(getProductDetail(id));
+    return () => {
+      dispatch(clean());
+    };
+  }, [dispatch, id]);
 
   const notify = () => toast.success("Item added to cart");
-    
-    const handleAddToCart= () => {
-      dispatch(addToCart(product.product.id))
-      notify()
-      }
 
+  const handleAddToCart = () => {
+    dispatch(addToCart(product.product.id));
+    notify();
+  };
 
-     if (product.length !== 0)
-     return (
+  if (product.length !== 0)
+    return (
       <div className={d.detail}>
-       <div className={d.container}>
-        <div className={d.backLink}><Link to={'/shop/'}>« Continue shopping</Link></div>
-         <div className={d.img}>
-           <img src={product.product.image}  />
-         </div>
-           <div className={d.content}>
-           <h1>{product.product.name} <a href="#">
-            <span class="material-symbols-outlined"> favorite
-</span></a> </h1> 
-           <p>Rating: {product.product.rating}</p>
-           <h2>${product.product.price}</h2>
-           <h3>{product.product.description}</h3>
-           <div className={d.button}>
-           <button onClick={()=> handleAddToCart()}> Add to cart 
-            <span class="material-symbols-outlined"> shopping_cart </span>
-            </button>
-           </div>
-         </div>
-         </div>
-       <div className={d.reviews}>
-       <h2>Reviews: </h2>
-           {product.product.reviews ? (
-             product.product.reviews.map((r) => (
-               <div className={d.reviewsContent}>
-                 <h3> {r} </h3>
-               </div>
-             ))
-           ) : (
-             <div className={d.reviewsContent} >
-               {" "}
-               <h3> This product does not have reviews yet. </h3>
-             </div>
-           )}
-          <div className={d.reviewsBtn}>
-            
-          <Link to="/reviews"> <button type="button" class="btn btn-danger" style={{width:"30%"}} data-toggle="modal" data-target="#form">Add reviews</button>
-          </Link>
-
+        <div className={d.container}>
+          <div className={d.backLink}>
+            <Link to={"/shop/"}>« Continue shopping</Link>
           </div>
-       </div>
-       <ToastContainer
+          <div className={d.img}>
+            <img src={product.product.image} />
+          </div>
+          <div className={d.content}>
+            <h1>
+              {product.product.name}{" "}
+              <a href="#">
+                <span class="material-symbols-outlined"> favorite</span>
+              </a>{" "}
+            </h1>
+            <p>Rating: {product.product.rating}</p>
+            <h2>${product.product.price}</h2>
+            <h3>{product.product.description}</h3>
+            <div className={d.button}>
+              <button onClick={() => handleAddToCart()}>
+                {" "}
+                Add to cart
+                <span class="material-symbols-outlined"> shopping_cart </span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={d.reviews}>
+          <h2>Reviews: </h2>
+          {product.product.reviews ? (
+            product.product.reviews.map((r) => (
+              <div className={d.reviewsContent}>
+                <h3> {r} </h3>
+              </div>
+            ))
+          ) : (
+            <div className={d.reviewsContent}>
+              {" "}
+              <h3> This product does not have reviews yet. </h3>
+            </div>
+          )}
+          <div className={d.reviewsBtn}>
+            <Link to="/reviews">
+              {" "}
+              <button
+                type="button"
+                class="btn btn-danger"
+                style={{ width: "30%" }}
+                data-toggle="modal"
+                data-target="#form"
+              >
+                Add reviews
+              </button>
+            </Link>
+          </div>
+        </div>
+        <ToastContainer
           position="bottom-right"
           autoClose={5000}
           hideProgressBar
@@ -81,12 +98,12 @@ export default function Detail(){
           theme="light"
         />
       </div>
-     );
-    else return (
-        <div>
-          <NavLink to='/reviews'>Reviews</NavLink>
-
-            Loading...
-        </div>
-    )
+    );
+  else
+    return (
+      <div>
+        <NavLink to="/reviews">Reviews</NavLink>
+        Loading...
+      </div>
+    );
 }
