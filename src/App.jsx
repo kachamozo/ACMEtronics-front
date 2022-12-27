cef7cbe388180c5c008544b9ab43785b142985da
ErikaJPB <erikajpinedab@gmail.com>
2022-12-27 16:32:20 -0500

    fix stripe showing

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Shop from "./components/Shop/Shop";
 import Detail from "./components/Detail/Detail";
 import Filter from "./components/Filter/Filter";
 import Form from "./components/Form/Form";
-import UpdateProduct from "./components/UpdateProductForm/UpdateProduct";
-import WishList from "./components/WishList/WishList.jsx";
+// import UpdateProduct from "./components/UpdateProductForm/UpdateProduct";
+// import WishList from "./components/WishList/WishList.jsx";
 import Footer from "./components/Footer/Footer";
 import Landing from "./components/Landing/Landing";
 import Cart from "./components/Cart/Cart";
@@ -16,12 +16,12 @@ import SignUp from "./components/SignUp/SignUp";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Reviews from "./components/Reviews/Reviews";
-import PrivateRoute from "./components/PrivateRoute";
-import { useSelector } from "react-redux";
-
+// import PrivateRoute from "./components/PrivateRoute";
+// import { useSelector } from "react-redux";
+import Stripe from "./components/Stripe/Stripe";
 function App() {
-  const actualUser = useSelector((state) => state.userDetail);
-  const isAllowed = !!actualUser.name;
+  // const actualUser = useSelector((state) => state.userDetail);
+  // const isAllowed = !!actualUser.name;
 
   return (
     <div>
@@ -40,16 +40,17 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/reviews" element={<Reviews />} />
+        <Route path="/stripe" element={<Stripe />} />
 
-        <Route
+        {/* <Route
           path="/createproduct/*"
           element={
             <PrivateRoute user={actualUser.admin}>
               <Form />
             </PrivateRoute>
           }
-        />
-
+        /> */}
+        {/* 
         <Route
           path="/updateproduct"
           element={
@@ -57,7 +58,7 @@ function App() {
               <UpdateProduct />
             </PrivateRoute>
           }
-        />
+        /> */}
       </Routes>
       <Footer />
     </div>
